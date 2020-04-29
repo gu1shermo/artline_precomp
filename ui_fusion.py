f6e6c1d11949beee5ff72d9a7d4ceb0c715cd1de
gcournet <gcournet@xilam.com>
2020-04-29 17:15:34 +0200

    signals connected, better display

--- ui_fusion.py
+++ ui_fusion.py
@@ -18,6 +18,7 @@ from PySide2.QtWidgets import (
     QGridLayout,
     QFileDialog,
     QSpinBox,
+    QFormLayout,
 )
 
 import pictures
@@ -33,29 +34,34 @@ class Widget(QWidget):
 
         self.btn_add_layer = QPushButton("Add layer")
 
-        self.btn_remove_layer = QPushButton("Remove layer")
-        self.btn_clear = QPushButton("Clear")
-        self.btn_run = QPushButton("Run")
+        self.btn_remove_layer = QPushButton("Remove 1 layer")
+        self.btn_clear = QPushButton("Clear All Layers")
 
         self.sbox_alpha = QSpinBox()
-        self.sbox_alpha.setRange(0,100)
+        self.sbox_alpha.setRange(0, 100)
         self.sbox_alpha.setValue(50)
 
         self.sbox_rgb = QSpinBox()
-
+        self.sbox_rgb.setRange(0, 100)
+        self.sbox_rgb.setValue(50)
 
         # connect buttons
         # Signals and Slots
         self.btn_add_layer.clicked.connect(self.add_layer)
 
         self.btn_remove_layer.clicked.connect(self.remove_layer)
-        self.btn_clear.clicked.connect(self.clear)
-        self.btn_run.clicked.connect(self.run_action)
+        self.sbox_remove_index = QSpinBox()
+        self.sbox_remove_index.setValue(0)
+        self.sbox_remove_index.setRange(0, 100)
+        self.sbox_remove_index.setSingleStep(1)
 
-        self.pic_1 = QLabel()
-        self.pic_2 = QLabel()
+        self.lbl_index_remove = QLabel("index calque")
+
+        self.btn_clear.clicked.connect(self.clear)
+
         self.pic_res = QLabel()
 
+        self.manager_img.pixmap_updated.connect(self.pic_res.setPixmap)
         # self.pic_1.setGeometry(QRect(10, 40, 500, 500))
         # self.pic_1.setGeometry(QRect(10, 540, 500, 500))
         # self.pic_1.setPixmap(pixmap)
@@ -66,33 +72,37 @@ class Widget(QWidget):
         self.cbox_definitions = QComboBox()
         self.setup_cbox()
 
+        self.form_layout = QFormLayout()
+        self.form_layout.addRow("alpha:", self.sbox_alpha)
+        self.form_layout.addRow("rgb:", self.sbox_rgb)
+
         self.cbox_actions.currentIndexChanged.connect(self.update_blendmode)
 
-        self.path = QLineEdit()
+        self.h_layout_add_layer = QHBoxLayout()
+        self.h_layout_add_layer.addWidget(self.btn_add_layer)
+        self.h_layout_add_layer.addWidget(self.cbox_actions)
+        self.h_layout_add_layer.addWidget(self.cbox_definitions)
+        self.h_layout_add_layer.addLayout(self.form_layout)
 
-        self.grid = QGridLayout()
+        self.h_layout_remove = QHBoxLayout()
+        self.h_layout_remove.addWidget(self.btn_remove_layer)
+        self.h_layout_remove.addWidget(self.lbl_index_remove)
+        self.h_layout_remove.addWidget(self.sbox_remove_index)
+        self.h_layout_remove.addWidget(self.btn_clear)
+
         self.v_layout = QVBoxLayout()
+        self.v_layout.addWidget(self.pic_res)
+        self.v_layout.addLayout(self.h_layout_add_layer)
+        # self.v_layout.addLayout(self.form_layout)
+        self.v_layout.addLayout(self.h_layout_remove)
 
-        self.v_layout.addWidget(self.btn_add_layer)
-
-        self.v_layout.addWidget(self.cbox_actions)
-        self.v_layout.addWidget(self.cbox_definitions)
-        self.v_layout.addWidget(self.sbox_alpha)
-        self.v_layout.addWidget(self.sbox_rgb)
-        self.v_layout.addWidget(self.path)
-        self.v_layout.addWidget(self.btn_remove_layer)
-        self.v_layout.addWidget(self.btn_clear)
-        self.v_layout.addWidget(self.btn_run)
-        self.grid.addWidget(self.pic_1, 0, 0)
-        self.grid.addWidget(self.pic_2, 0, 1)
-        self.grid.addLayout(self.v_layout, 1, 0)
-        self.grid.addWidget(self.pic_res, 1, 1)
-
-        self.setLayout(self.grid)
+        self.setLayout(self.v_layout)
 
     def remove_layer(self):
-        
-        self.manager_img.remove_layer(len(self.layers) - 1)
+        self.manager_img.remove_layer(self.sbox_remove_index.value())
+        self.layers.remove(self.sbox_remove_index.value())
+
+        # self.sbox_remove_index.setValue(self.sbox_remove_index.value() - 1)
 
     def clear(self):
         self.layers = []
@@ -119,14 +129,16 @@ class Widget(QWidget):
     def add_layer(self):
         path_filename = QFileDialog.getOpenFileName(self, "Choose Image", "/home/",)
 
+        #self.manager_img.set_definition(self.cbox_definitions.currentText())
         self.manager_img.add_layer(
             path_filename[0], blend_mode=self.cbox_actions.currentText()
         )
 
         pixmap = QPixmap(path_filename[0])
-        self.pic_1.setPixmap(pixmap)
+        self.pic_res.setPixmap(pixmap)
 
         self.layers.append(pixmap)
+        # self.sbox_remove_index.setValue(self.sbox_remove_index.value() + 1)
 
     def load_img_2(self):
         path_filename = QFileDialog.getOpenFileName(
